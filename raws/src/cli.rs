8f8ab8a30cc0bbe3012afdbfea784b28752d895a
Cyril Plisko <cyril.plisko@mountall.com>
2023-05-29 00:30:54 +0300

    Display global args last in the help

--- raws/src/cli.rs
+++ raws/src/cli.rs
@@ -4,19 +4,19 @@ use super::*;
 #[command(version, about, long_about = None)]
 pub(crate) struct Cli {
     /// Debug data representation
-    #[arg(long, global = true)]
+    #[arg(long, global = true, display_order = 1000010)]
     debug: bool,
 
     /// Use a specific profile from your credential file.
-    #[arg(long, global = true)]
+    #[arg(long, global = true, display_order = 1000020)]
     profile: Option<String>,
 
     /// The region to use. Overrides config/env settings.
-    #[arg(long, global = true)]
+    #[arg(long, global = true, display_order = 1000030)]
     region: Option<String>,
 
     /// The formatting style for the command output.
-    #[arg(long, global = true, value_enum)]
+    #[arg(long, global = true, value_enum, display_order = 1000040)]
     output: Option<config::Output>,
 
     #[command(subcommand)]
